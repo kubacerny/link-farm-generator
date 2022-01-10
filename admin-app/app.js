18f50e81c8672d64a2efbb957680910b4c7f44f9
Jakub Cerny <jacerny@gmail.com>
2022-01-10 11:33:17 +0100

    TASK - increase payload limit to 20mb

--- admin-app/app.js
+++ admin-app/app.js
@@ -36,8 +36,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ extended: false, limit: '20mb' }));
 app.use(cookieParser());
 
 app.use(compression());
@@ -59,12 +59,12 @@ app.use('/', indexRouter);
 app.use('/admin', requiresAuth(), adminRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
