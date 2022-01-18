1cef9a32547611f000bdb81cc49050aa44468983
Jakub Cerny <jacerny@gmail.com>
2022-01-18 12:42:13 +0100

    BUGFIX - cannot logout without permissions

--- admin-app/routes/index.js
+++ admin-app/routes/index.js
@@ -3,17 +3,17 @@ var router = express.Router();
 
 var configProvider = require('../../common-lib/configProvider');
 
-function hasPermission(configJSON, user) {
-  return configJSON.allowedEmails && user.email && configJSON.allowedEmails.indexOf(user.email) >= 0;
+function hasPermissionFun(isAuthenticated, configJSON, user) {
+  return isAuthenticated && configJSON.allowedEmails && user.email && configJSON.allowedEmails.indexOf(user.email) >= 0;
 }
 
 /* GET home page. */
 router.get('/', function(req, res, next) {  
-  let isAuthenticated = req.oidc.isAuthenticated();
-  isAuthenticated = isAuthenticated && hasPermission(configProvider.get(), req.oidc && req.oidc.user);
+  const isAuthenticated = req.oidc.isAuthenticated();
+  const hasPermission = hasPermissionFun(isAuthenticated, configProvider.get(), req.oidc && req.oidc.user);
   const userName = req.oidc && req.oidc.user && req.oidc.user.name;
 
-  res.render('index', { isAuthenticated, userName, title: 'Administration Homepage' });
+  res.render('index', { isAuthenticated, hasPermission, userName, title: 'Administration Homepage' });
 });
 
 module.exports = router;
